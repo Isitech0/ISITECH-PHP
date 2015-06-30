149afccda93a453123013dbd425c73cefd2af1c5
Ali <aliloubel@live.fr>
2015-06-30 11:23:55 +0200

    create admin bundle

---
+++ isitech_php/src/isitech_php/AdminBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace isitech_php\AdminBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/hello/Fabien');
+
+        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+    }
+}
---
+++ isitech_php/src/isitech_php/AdminBundle/isitech_phpAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace isitech_php\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class isitech_phpAdminBundle extends Bundle
+{
+}
