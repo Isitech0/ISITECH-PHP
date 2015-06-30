17ee181b718fd07d3c4994c5228c62f67de93199
Lightgin <xx-white-dragoon-xx@hotmail.fr>
2015-06-30 10:02:49 +0200

    maj controller

--- isitech_php/src/Sample/AdminBundle/Controller/LoggedController.php
+++ isitech_php/src/Sample/AdminBundle/Controller/LoggedController.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Etudiant
- * Date: 30/06/2015
- * Time: 09:57
- */
+
+namespace Site\GeneralBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class Logged extends Controller
+{
+    public function foo(){
+        return new Response('Hello world!');
+    }
+    public function listing(){
+        //retourne une liste de produits
+        //appelle constructeur produit
+    }
+    public function ordered(){
+        
+    }
+}
