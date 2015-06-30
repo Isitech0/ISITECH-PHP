51b0f79ae0b46fa73165c601ee81123f69b41223
alandrieu <alexis.landrieu.pro@gmail.com>
2015-06-30 16:09:44 +0200

    Vue Article.

--- isitech_php/src/isitechphp/MainBundle/Controller/DefaultController.php
+++ isitech_php/src/isitechphp/MainBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace isitechphp\MainBundle\Controller;
 
+use isitechphp\MainBundle\Bean\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -9,11 +10,20 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/hellov2")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        //$articles = array();
+        $unarticle = new Article();
+
+        $unarticle->id = 1;
+        $unarticle->nom = "Shit";
+        $unarticle->description = "pas bien";
+
+        $articles = array($unarticle);
+
+        return $this->render('isitechphpMainBundle:Default:ArticleView.html.twig', array('articles' => $articles));
     }
 }
